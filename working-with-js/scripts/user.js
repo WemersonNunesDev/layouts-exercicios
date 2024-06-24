58188f8b79c18bbfb69957ab23afc9a4bc4e59c4
WemersonNunesDev <wemersononunes@gmail.com>
2024-06-24 16:13:55 -0300

    Error
    Eu não consigo mais T_T

--- working-with-js/scripts/user.js
+++ working-with-js/scripts/user.js
@@ -1,20 +1,18 @@
-const res = document.querySelector('div.resultado');
-const nomeUser = document.getElementById('nome');
-const idadeUser = document.getElementById('idade');
-const alturaUser = document.getElementById('altura');
-const pesoUser = document.getElementById('peso');
-
-
-const nome = String(nomeUser.value);
-const idade = Number(idadeUser.value);
-const altura = Number(alturaUser.value);
-const peso = Number(pesoUser.value);
-const imc = Number(peso / (altura * altura)).toFixed(2);
-
 function calcular() {
-    res.innerHTML = `<p>
-    Nome: ${nome}
-    Idade: ${idade} anos
-    IMC: ${imc}
-    </p>`
+    const res = document.querySelector('div.resultado');
+    const nome = document.querySelector('input#nome');
+    const idadeUser = document.querySelector('input#idade');
+    const alturaUser = document.querySelector('input#altura');
+    const pesoUser = document.querySelector('input#peso');
+
+    const idade = Number(idadeUser.value);
+    const altura = Number(alturaUser.value);
+    const peso = Number(pesoUser.value);
+    const imc = peso / (altura * altura);
+
+
+    res.innerHTML = `
+    <p>${nome}</p>
+    <p>${idade} anos.</p>
+    <p>Seu IMC é ${imc.toFixed(2)}</p>`
 }
